3c96e36fbb09b3defd94401464ffc0856a10199d
asit6371 <asitpanda577@gmail.com>
2026-04-27 21:32:14 +0530

    Initial commit

---
+++ src/test/java/com/onlineassessment/OnlineAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlineassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
