f4637433899ac6bd4aa918c7474a812c74aa9054
asit6371 <asitpanda577@gmail.com>
2026-06-06 15:33:41 +0530

    add admin initializer

---
+++ src/main/java/com/onlineassessment/config/AdminInitializer.java
@@ -0,0 +1,46 @@
+package com.onlineassessment.config;
+
+import com.onlineassessment.entity.User;
+import com.onlineassessment.enums.RoleEnum;
+import com.onlineassessment.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AdminInitializer implements CommandLineRunner {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public AdminInitializer(
+            UserRepository userRepository,
+            PasswordEncoder passwordEncoder
+    ) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public void run(String... args) {
+
+        String adminEmail = "admin@assessmate.com";
+
+        if (!userRepository.existsByEmail(adminEmail)) {
+
+            User admin = new User();
+
+            admin.setName("Admin");
+            admin.setEmail(adminEmail);
+            admin.setPassword(
+                    passwordEncoder.encode("admin123")
+            );
+
+            admin.setRole(RoleEnum.ADMIN.ADMIN);
+
+            userRepository.save(admin);
+
+            System.out.println("Admin user created successfully.");
+        }
+    }
+}
