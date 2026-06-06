243e44a764a690a663b731c9923ff4a01eda2e8e
asit6371 <asitpanda577@gmail.com>
2026-06-06 19:30:40 +0530

    fix cors for vercel frontend

--- src/main/java/com/onlineassessment/config/CorsConfig.java
+++ src/main/java/com/onlineassessment/config/CorsConfig.java
@@ -20,7 +20,7 @@ public class CorsConfig {
 
         configuration.setAllowedOrigins(
                 List.of(
-                        "http://localhost:5173"
+                        "https://judgex-app.vercel.app"
                 )
         );
 
